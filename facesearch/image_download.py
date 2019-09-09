bc42f6011dc2dec17bb08571240f02eb17b03e90
Junichi Sato <sat0b.dev@gmail.com>
2019-09-10 07:13:56 +0900

    Fix to make save directory automatically fix warning log to show the reason

--- facesearch/image_download.py
+++ facesearch/image_download.py
@@ -1,12 +1,13 @@
 import argparse
 import json
+import os
 
 import bs4
 import requests
 from loguru import logger
 
 
-def download_image(query):
+def download_image(query, save_directory):
     url = "https://www.google.co.jp/search?q=" + query + "&source=lnms&tbm=isch"
     header = {
         'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
@@ -18,20 +19,23 @@ def download_image(query):
         image_url = json.loads(a.text)['ou']
         try:
             res = requests.get(image_url)
-            image_name = './static/image/{}.jpg'.format(i)
+            image_name = './{}/{}.jpg'.format(save_directory, i)
             with open(image_name, 'wb') as f:
                 f.write(res.content)
                 logger.info("Save: {}".format(image_name))
-        except:
-            logger.warning("skip " + image_url)
+        except Exception as e:
+            logger.warning("skip {}, {}".format(url, e))
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('query', default="芸能人")
+    parser.add_argument('--save-directory', default="static/image")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    download_image(args.query)
+    if not os.path.exists(args.save_directory):
+        os.makedirs(args.save_directory)
+    download_image(args.query, args.save_directory)
