eb41c126d9b9574724e1a9acf82a36f010dffd35
Junichi Sato <sat0b.dev@gmail.com>
2019-09-19 07:46:49 +0900

    Add the base of watadare webapp

---
+++ webapp/src/js/client.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <h1>watadare</h1>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById('app')
+);
